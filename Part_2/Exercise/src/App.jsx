055e6ae62b925547430d0bfbe401c7083120ce4d
KamranTalpur <kamrantalpur646@gmail.com>
2025-09-26 23:23:21 +0500

    Exercise 2.1 to 2.10 Done

---
+++ Part_2/Exercise/src/App.jsx
@@ -0,0 +1,47 @@
+import React, { use } from "react";
+import { useState } from "react";
+
+function App(props) {
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState("");
+  const [showAll, setShowAll] = useState(true);
+
+  const handleNoteChange = (e) => {
+    setNewNote(e.target.value);
+  };
+
+  const addNote = (e) => {
+    e.preventDefault();
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
+    };
+    setNotes(notes.concat(noteObject));
+    setNewNote("");
+  };
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important);
+  return (
+    <div>
+      <h1>Notes</h1>
+      <button onClick={() => setShowAll(!showAll)}>
+        show {showAll ? "important" : "all"}
+      </button>
+      <ul>
+        {notesToShow.map((note) => (
+          <li key={note.id}>{note.content}</li>
+        ))}
+      </ul>
+      <form onSubmit={addNote}>
+        <input
+          placeholder="Type Notes Here..."
+          value={newNote}
+          onChange={handleNoteChange}
+        />
+        <button type="submit">Save</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
---
+++ Part_2/Exercise2.1To2.5/src/components/Course.jsx
@@ -0,0 +1,18 @@
+export default function Course({ course }) {
+  return (
+    <div>
+      <h1>{course.name}</h1>
+      {course.parts.map((part) => (
+        <p key={part.id}>
+          {part.name} {part.exercises}
+        </p>
+      ))}
+      <p>
+        <strong>
+          total of {course.parts.reduce((sum, part) => sum + part.exercises, 0)}{" "}
+          exercises
+        </strong>
+      </p>
+    </div>
+  );
+}
